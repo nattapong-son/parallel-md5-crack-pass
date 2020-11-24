73a148b76dc3d598e696d30f921e4d87f5e74bdf
nattapong-son <66114040+nattapong-son@users.noreply.github.com>
2020-11-24 20:00:16 +0700

    Fix md5

--- md5.c
+++ md5.c
@@ -3,61 +3,58 @@
 #include <string.h>
 #include <omp.h>
 #include <openssl/md5.h>
- 
+
 typedef unsigned char byte;
- 
-int matches(byte *a, byte* b) {
-	for (int i = 0; i < 32; i++)
-		if (a[i] != b[i])
-			return 0;
-	return 1;
+
+void MD5_HashString(char *src, char *dest){
+  MD5_CTX ctx;
+  unsigned char digest[16];
+  int i;
+  MD5_Init(&ctx);
+  MD5_Update(&ctx, src, strlen(src));
+  MD5_Final(digest,&ctx);
+  for (i = 0; i < 16; i++)  {
+    sprintf(&dest[i*2],"%02x", digest[i]);
+  }
 }
- 
- 
-byte* StringHashToByteArray(const char* s) {
-	byte* hash = (byte*) malloc(32);
-	char two[3];
-	two[2] = 0;
-	for (int i = 0; i < 32; i++) {
-		two[0] = s[i * 2];
-		two[1] = s[i * 2 + 1];
-		hash[i] = (byte)strtol(two, 0, 16);
-	}
-	return hash;
-}
- 
-void printResult(byte* password, byte* hash) {
-	char sPass[6];
-	memcpy(sPass, password, 5);
-	sPass[5] = 0;
-	printf("%s => ", sPass);
-	for (int i = 0; i < MD5_DIGEST_LENGTH; i++)
-		printf("%02x", hash[i]);
-	printf("\n");
-}
- 
+
 int main(int argc, char **argv)
 {
- 
+
 #pragma omp parallel
 	{
- 
-#pragma omp for
-		for (int a = 0; a < 26; a++)
-		{
-			byte password[5] = { 97 + a };
-			byte* one =   StringHashToByteArray("95dbb48a7b21a9cff9221eb0b431fa3c");
-			for (password[1] = 97; password[1] < 123; password[1]++)
-				for (password[2] = 97; password[2] < 123; password[2]++)
-					for (password[3] = 97; password[3] < 123; password[3]++)
-						for (password[4] = 97; password[4] < 123; password[4]++) {
-							byte *hash = MD5(password, 5, 0);
-							if (matches(one, hash))
-								printResult(password, hash);
-						}
-			free(one);
-		}
+		#pragma omp for
+			for (int i = 0 ; i <= 9; i++) {
+					for (int j = 0 ; j <= 9; j++) {
+							for (int k = 0 ; k <= 9; k++) {
+									for (int l = 0 ; l <= 9; l++) {
+										for (int m = 0 ; m <= 9; m++) {
+											for (int n = 0 ; n <= 9; n++) {
+												for (int o = 0 ; o <= 9; o++) {
+													for (int p = 0 ; p <= 9; p++) {
+														char password[] = { '0' + i, '0' + j, '0' + k, '0' + l, '0' + m, '0' + n, '0' + o, '0' + p  };
+
+														// hash
+														char passwordMD5[32];
+														MD5_HashString(password, passwordMD5);
+
+														// compare
+														if (strcmp("95dbb48a7b21a9cff9221eb0b431fa3c", passwordMD5) == 0) {
+															printf("Found!\nPassword is ");
+															for (int q = 0 ; q < 8 ; q++) {
+																printf("%d", copyOfPassword[q]);
+															}
+															printf("\n");
+														}
+													}
+												}
+											}
+										}
+									}
+							}
+					}
+			}
 	}
- 
+
 	return 0;
 }
